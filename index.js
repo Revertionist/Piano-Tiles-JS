6be5eb694286dda2658ca565fb810abc894b78b5
revertionist <govindrnair.rev@gmail.com>
2023-09-17 22:53:19 +0530

    WIP: ramdom generated tiles

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const squares = document.querySelectorAll('.square');
+const colorChange = "lightgreen";
+const revertColor = "#ccc"
+let rotation = 0;
+
+while (rotation < 5) {
+    function changeColor() {
+
+        let randomSquare = Math.floor(Math.random() * squares.length);
+
+        squares[randomSquare].style.backgroundColor = colorChange;
+
+        setTimeout(() => {
+            squares[randomSquare].style.backgroundColor = revertColor;
+        }, 1000);
+
+    }
+    rotation++;
+    changeColor();
+
+}
+
+
+changeColor();
+
+
 
 squares.forEach((square) => {
     square.addEventListener('click', () => {
-        const colorChange = "lightgreen";
         square.style.backgroundColor = colorChange;
     })
 })
