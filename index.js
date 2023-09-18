241df25319a67bb28bc2413d2082883bb158168e
revertionist <govindrnair.rev@gmail.com>
2023-09-18 21:42:52 +0530

    fix: unique squares

--- index.js
+++ index.js
@@ -2,26 +2,45 @@ const squares = document.querySelectorAll('.square');
 const colorChange = "lightgreen";
 const revertColor = "#ccc"
 let rotation = 0;
-
+let checkRotation = 0;
+let isUnique = true;
+let Progress = true;
+let originalSequence = [];
 
 function changeColor() {
-
+    checkRotation = 0;
+    isUnique = true;
     let randomSquare = Math.floor(Math.random() * squares.length);
 
-    setTimeout(() => {
-        squares[randomSquare].style.backgroundColor = colorChange;
-    }, 2500);
+    while (checkRotation < rotation) {
+        if (originalSequence[checkRotation] == randomSquare) {
+            isUnique = false;
+            break;
+        } else {
+            checkRotation++;
+        }
+    }
+    
 
-    setTimeout(() => {
-        squares[randomSquare].style.backgroundColor = revertColor;
-    }, 3000);
+    if (isUnique == true) {
+        originalSequence [rotation] = randomSquare;
+        setTimeout(() => {
+            squares[randomSquare].style.backgroundColor = colorChange;
+        }, 2500);
 
-    rotation++;
+        setTimeout(() => {
+            squares[randomSquare].style.backgroundColor = revertColor;
+        }, 3000);
 
-    setTimeout(() => {
-        if (rotation < 5)
-            changeColor();
-    }, 1000);
+        rotation++;
+
+        setTimeout(() => {
+            if (rotation < 5)
+                changeColor();
+        }, 1000);
+    } else {
+        changeColor();
+    }
 }
 
 changeColor();
