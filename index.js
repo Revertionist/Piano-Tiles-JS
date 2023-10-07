164cea8ff57d1d111cdc411c18d5e7dbb8b6ba6e
revertionist <govindrnair.rev@gmail.com>
2023-10-07 21:16:27 +0530

    feat: game results

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const squares = document.querySelectorAll('.square');
 const colorChange = "lightgreen";
-const revertColor = "#ccc"
+const revertColor = "#ccc";
 let rotation = 0;
 let clickRotation = 0;
 let checkRotation = 0;
@@ -8,6 +8,8 @@ let isUnique = true;
 let Progress = true;
 let originalSequence = [];
 let clickedSequence = [];
+let win = true;
+let clickEnable = false;
 
 function changeColor() {
     checkRotation = 0;
@@ -39,6 +41,8 @@ function changeColor() {
         setTimeout(() => {
             if (rotation < 5)
                 changeColor();
+            else
+                clickEnable = true;
         }, 1000);
     } else {
         changeColor();
@@ -48,13 +52,36 @@ function changeColor() {
 changeColor();
 
 
+
 squares.forEach((square, index) => {
     square.addEventListener('click', () => {
+        console.log(clickEnable)
+        if (!clickEnable) return;
+
         square.style.backgroundColor = colorChange;
         clickedSequence[clickRotation] = index;
         clickRotation++;
+
+        if (clickedSequence.length === originalSequence.length)
+            check();
     })
 })
 
-console.log (originalSequence);
-console.log (clickedSequence);
+
+
+
+function check() {
+    for (let i = 0; i < clickedSequence.length; i++) {
+        console.log(originalSequence[i], clickedSequence[i])
+        if (originalSequence[i] != clickedSequence[i]) {
+            win = false;
+            break;
+        }
+    }
+
+    if (win) {
+        window.open('pages/win.html', '_self');
+    } else {
+        window.open('pages/lose.html', '_self');
+    }
+}
